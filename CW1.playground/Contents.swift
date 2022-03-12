1b9ce5311e194d2d11b0c42841568d7d29472012
shaikaalobaid <salobaid250@gmail.com>
2022-03-12 11:19:33 +0300

    done

--- CW1.playground/Contents.swift
+++ CW1.playground/Contents.swift
@@ -3,3 +3,11 @@ import UIKit
 // التمرين الأول
 // ClassWork 1
 
+var name = "shaika"
+var birthdayyear = 2005
+var age = 2022 - (birthdayyear)
+print("hello my name is \(name) and my age \(age)")
+var string = "8"
+var int = 4
+var double = 2.5
+var stringint = Double(string)! + Double(int)
